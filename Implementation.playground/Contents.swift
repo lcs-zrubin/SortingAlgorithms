61b920639bc3d1452218144117d8dcbdf0843db3
Russell Gordon <rgordon@lcs.on.ca>
2018-02-20 08:27:11 -0500

    Added example to show appending to an array

--- Implementation.playground/Contents.swift
+++ Implementation.playground/Contents.swift
@@ -3,12 +3,7 @@ import Foundation
  # Sorting Algorithms
  This purpose of this playground is to provide the *tools* (example code) and the *space* to help you with the implementation of the sorting algorithm you have devised with your partner.
  ## Example Code
- What follows is a series of examples showing how to do common operations with an array in Swift.
- ### Creating an empty array
  */
-// Note the use of square brackets around the data type when the array is declared
-var numbers : [Int] = []
-
 //: ### Random number generation
 // Generates a random number between the minimum and maximum values given (inclusive)
 func random(between min: UInt32, and max: UInt32) -> Int {
@@ -16,6 +11,15 @@ func random(between min: UInt32, and max: UInt32) -> Int {
 }
 
 // Invoking (using) the function above
-random(between: 1, and: 10)
+random(between: 1, and: 50)
 
+//: ### Creating an empty array
+// Note the use of square brackets around the data type when the array is declared
+var numbers : [Int] = []
 
+//: ### Adding to the end of an array
+// A single random number is added to the end of the array
+numbers.append( random(between: 1, and: 50) )
+
+// Add another random number to the end of the array
+numbers.append( random(between: 1, and: 50) )
